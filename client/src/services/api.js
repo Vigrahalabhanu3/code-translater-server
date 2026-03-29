be1cf2096ccc8b44803e978fcea8196b258e8603
Bhanu <bhanu@example.com>
2026-03-29 16:04:04 +0530

    Update API baseURL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://code-translater-server-git-323310668908.asia-south1.run.app/api",
 });
 
 API.interceptors.request.use((config) => {
